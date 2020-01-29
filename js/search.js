2a6a0bb3879f1d7d23a7101140c1bdd3f2cd77d8
ci-build <antoniogamiz@gmail.com>
2020-01-29 11:29:48 +0000

    [skip ci] Updates

--- js/search.js
+++ js/search.js
@@ -119,6 +119,12 @@ $(function(){
 ,
 { category: "enum", value: "Kind", url: "/Documentable/type/Kind" }
 ,
+{ category: "class", value: "Documentable::DocPage::Factory", url: "/Documentable/type/Documentable::DocPage::Factory" }
+,
+{ category: "class", value: "Documentable::Index", url: "/Documentable/type/Documentable::Index" }
+,
+{ category: "class", value: "Documentable::Registry", url: "/Documentable/type/Documentable::Registry" }
+,
 { category: "role", value: "Documentable::DocPage", url: "/Documentable/type/Documentable::DocPage" }
 ,
 { category: "class", value: "Documentable::DocPage::Primary::Type", url: "/Documentable/type/Documentable::DocPage::Primary::Type" }
@@ -141,6 +147,14 @@ $(function(){
 ,
 { category: "class", value: "Documentable::DocPage::Index::Programs", url: "/Documentable/type/Documentable::DocPage::Index::Programs" }
 ,
+{ category: "class", value: "Documentable::Config", url: "/Documentable/type/Documentable::Config" }
+,
+{ category: "class", value: "X::Documentable::Config::InvalidConfig", url: "/Documentable/type/X::Documentable::Config::InvalidConfig" }
+,
+{ category: "class", value: "Documentable::Search", url: "/Documentable/type/Documentable::Search" }
+,
+{ category: "class", value: "Documentable::Secondary", url: "/Documentable/type/Documentable::Secondary" }
+,
 { category: "class", value: "Documentable::Primary", url: "/Documentable/type/Documentable::Primary" }
 ,
 { category: "class", value: "X::Documentable::TitleNotFound", url: "/Documentable/type/X::Documentable::TitleNotFound" }
@@ -151,33 +165,19 @@ $(function(){
 ,
 { category: "class", value: "X::Documentable::TypeNotFound", url: "/Documentable/type/X::Documentable::TypeNotFound" }
 ,
-{ category: "class", value: "Documentable::Search", url: "/Documentable/type/Documentable::Search" }
-,
-{ category: "class", value: "Documentable::Registry", url: "/Documentable/type/Documentable::Registry" }
-,
-{ category: "class", value: "Documentable::Index", url: "/Documentable/type/Documentable::Index" }
-,
-{ category: "class", value: "Documentable::Secondary", url: "/Documentable/type/Documentable::Secondary" }
-,
-{ category: "class", value: "Documentable::Config", url: "/Documentable/type/Documentable::Config" }
-,
-{ category: "class", value: "X::Documentable::Config::InvalidConfig", url: "/Documentable/type/X::Documentable::Config::InvalidConfig" }
-,
-{ category: "class", value: "Documentable::DocPage::Factory", url: "/Documentable/type/Documentable::DocPage::Factory" }
-,
 { category: "grammar", value: "Documentable::Heading::Grammar", url: "/Documentable/type/Documentable::Heading::Grammar" }
 ,
 { category: "class", value: "Documentable::To::HTML::Wrapper", url: "/Documentable/type/Documentable::To::HTML::Wrapper" }
 ,
-{ category: "Language", value: "Configuration file", url: "/Documentable/language/configuration-file" }
+{ category: "Language", value: "Perl6 Highlighter", url: "/Documentable/language/highlighting" }
 ,
-{ category: "Language", value: "TypeGraph representations", url: "/Documentable/language/typegraph" }
+{ category: "Language", value: "Configuration file", url: "/Documentable/language/configuration-file" }
 ,
 { category: "Language", value: "Pod6 Format", url: "/Documentable/language/document-format" }
 ,
-{ category: "Language", value: "Command line interface", url: "/Documentable/language/cli" }
+{ category: "Language", value: "TypeGraph representations", url: "/Documentable/language/typegraph" }
 ,
-{ category: "Language", value: "Perl6 Highlighter", url: "/Documentable/language/highlighting" }
+{ category: "Language", value: "Command line interface", url: "/Documentable/language/cli" }
 ,
 { category: "Routine", value: "BUILD", url: "/Documentable/routine/BUILD" }
 ,
